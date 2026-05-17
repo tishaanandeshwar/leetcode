581b93945011c7045fa8fb8779ca96e2b7f0e92c
tishaanandeshwar <tishanandeshwar049@gmail.com>
2026-05-17 21:37:52 +0530

    .
    Implement Two Sum algorithm to find indices of two numbers that add up to a target.

---
+++ TwoSum.py
@@ -0,0 +1,10 @@
+#1. Two Sum 
+class Solution:
+    def twoSum(self, nums, target):
+        prevMap = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+        return []
